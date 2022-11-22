4676f1c17dee9c67c0f04cbac76fe4d2ccbf5814
William Owen <william.owen.bsc@gmail.com>
2022-11-22 13:40:48 -0800

    edits for content

--- src/views/EnvSci.vue
+++ src/views/EnvSci.vue
@@ -1,32 +1,53 @@
 <template>
-<div class="local">
-    <h1>Local Knowledge</h1>
-  
+    <div class="local">
+        <h1>Local Knowledge</h1>
 
-    <p>I want to give you ideas about what you could study in preparation for local knowledge and make it easy to find a variety of material. We should all know a handful of local natural facts to share with guests.</p>
+        <p>
+            I want to give you ideas about what
+            you could study in preparation for
+            local knowledge and make it easy to
+            find a variety of material. We should
+            all know a handful of local natural
+            facts to share with guests.
+        </p>
 
-    <p>I don't describe any topics in detail here, but lists them out and link them to google searches (unless otherwise noted). Don't expect it to cover everything. I hope it help fuel your motivation as a guide.</p>
+        <p>
+            I don't describe any topics in detail
+            here, but lists them out and link them
+            to google searches (unless otherwise
+            noted). Don't expect it to cover
+            everything. I hope it help fuel your
+            motivation as a guide.
+        </p>
 
-    <br>
+        <br />
 
-    <p style="text-align: center">I'll add to this periodically. I break it down between these two categories:</p>
+        <p style="text-align: center">
+            I'll add to this periodically. I break
+            it down between these two categories:
+        </p>
 
-    <div class="breakdown">
-        <p>The <router-link to="/env-sci/geology">Geography</router-link> of the area </p>
-        <p>&</p>
-        <p><router-link to="/env-sci/ecology">Ecology</router-link> -- Plants & Animals </p>
+        <div class="breakdown">
+            <p>
+                The
+                <router-link to="/env-sci/geology"
+                    >Geography</router-link
+                >
+                of the area
+            </p>
+            <p>&</p>
+            <p>
+                <router-link to="/env-sci/ecology"
+                    >Ecology</router-link
+                >
+                -- Plants & Animals
+            </p>
+        </div>
     </div>
-
- 
-    
-</div>
-
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
@@ -38,5 +59,4 @@ export default {
     flex-direction: column;
     align-items: center;
 }
-
 </style>
--- src/views/TripPlanning.vue
+++ src/views/TripPlanning.vue
@@ -1,43 +1,62 @@
 <template>
-<div class="trip-planning">
-  <h1>Trip Planning</h1>
+    <div class="trip-planning">
+        <h1>Trip Planning</h1>
 
-  <p>This page has you covered for resources to help you plan a trip. There's a lot to unpack, but it'll become intuitive with practice.</p>
+        <p>
+            This page has you covered for
+            resources to help you plan a trip.
+            There's a lot to unpack, but it'll
+            become intuitive with practice.
+        </p>
 
-  <p>Click the topics to explore resources.</p>
+        <p>
+            Click the topics to explore resources.
+        </p>
 
-  <h2>Trip Checklist</h2>
+        <h2>Trip Checklist</h2>
 
-  <ul>
-     <li> <router-link :to="{ name: 'TripPlan', params: { id: 'Tides and Currents'}}">Tides and Currents</router-link></li>
-      <li><router-link :to="{ name: 'TripPlan', params: { id: 'Weather'}}">Weather</router-link></li>
-      <li><router-link :to="{ name: 'TripPlan', params: { id: 'Packing Checklist'}}">Packing Checklist</router-link></li>
-  </ul>
-
-  <h2>Group Management</h2>
-
-  <ul>
-      <li><router-link :to="{ name: 'TripPlan', params: { id: 'General Expectations'}}">General Expectations</router-link></li>
-      <li><router-link :to="{ name: 'TripPlan', params: { id: 'Edge-Cases'}}">Edge-Cases</router-link></li>
-  </ul>
-
-  <h2>VHF Radio</h2>
-  <ul>
-      <li><router-link :to="{ name: 'TripPlan', params: { id: 'Weather' }}">Weather</router-link></li>
-      <li><router-link :to="{ name: 'TripPlan', params: { id: 'Radio Calls' }}">Calls</router-link></li>
-  </ul>
-  
-  <h2> Risk Management </h2>
-    
-</div>
+        <ul>
+            <li>
+                <router-link
+                    :to="{
+                        name: 'TripPlan',
+                        params: {
+                            id:
+                                'Tides and Currents',
+                        },
+                    }"
+                    >Tides and
+                    Currents</router-link
+                >
+            </li>
+            <li>
+                <router-link
+                    :to="{
+                        name: 'TripPlan',
+                        params: { id: 'Weather' },
+                    }"
+                    >Weather</router-link
+                >
+            </li>
+            <li>
+                <router-link
+                    :to="{
+                        name: 'TripPlan',
+                        params: {
+                            id:
+                                'Packing Checklist',
+                        },
+                    }"
+                    >Packing
+                    Checklist</router-link
+                >
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
